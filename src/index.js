38a63e1b0325f9059a3eda84c29d891eb6e19ee6
MankovskiPavel <mankovskipavel@gmail.com>
2021-02-13 17:47:13 +0300

    MySolution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
 exports.min = function min (array) {
-  return 0;
+  
+  if ((array==null) || (array.length==0)) {return 0}
+  let nmin=array[0]
+  for (let i=1;i<array.length;i++)
+{
+  if (array[i]<nmin){
+    nmin=array[i]
+  }
+}  
+  return nmin;
 }
 
 exports.max = function max (array) {
-  return 0;
+  
+  if ((array==null) || (array.length==0)) {return 0}
+  let nmax=array[0]
+  for (let i=1;i<array.length;i++)
+{
+  if (array[i]>nmax){
+    nmax=array[i]
+  }
+}  
+  return nmax;
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if ((array==null) || (array.length==0)) {return 0}
+  let sum=0
+  for (let i=0;i<array.length;i++)
+{
+  sum+=array[i]
+  }
+let avr=sum/array.length
+  return avr;
+  }
